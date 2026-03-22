d37ac634445b7e05142a58c7be677c295136d095
Elliott Butler <2555214+elliottmb@users.noreply.github.com>
2026-03-21 21:13:50 -0500

    partial

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,40 @@
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ['./src/**/*.{html,js}', './demo/**/*.html'],
   theme: {
     extend: {
       colors: {
+        // Vibrant magazine colors - 70s-90s inspired
         vibrantBlue: '#007acc',
         vibrantPink: '#d5006d',
         vibrantOrange: '#ff6200ea',
         vibrantGreen: '#32cd32',
         vibrantYellow: '#ffeb3b',
+        
+        // Extended retro palette
+        burnished: {
+          50: '#faf8f5',
+          900: '#2d1810',
+        },
+        sunset: {
+          50: '#fef2e8',
+          500: '#ff6b35',
+          900: '#8b2e00',
+        },
+        acid: {
+          50: '#f0ff00',
+          500: '#ccff00',
+          900: '#b3cc00',
+        },
+        magenta: {
+          50: '#ffe0f7',
+          500: '#ff00ff',
+          900: '#cc00cc',
+        },
+        peacock: {
+          50: '#e0f7ff',
+          500: '#00bfff',
+          900: '#0066cc',
+        },
       },
       fontFamily: {
         playfair: ['Playfair Display', 'serif'],
@@ -16,6 +43,22 @@ module.exports = {
         roboto: ['Roboto', 'sans-serif'],
         abril: ['Abril Fatface', 'serif'],
         ibmPlex: ['IBM Plex Serif', 'serif'],
+        spaceMono: ['Space Mono', 'monospace'],
+        garamond: ['EB Garamond', 'serif'],
+        crimson: ['Crimson Text', 'serif'],
+        bodoni: ['Bodoni Moda', 'serif'],
+        raleway: ['Raleway', 'sans-serif'],
+        righteous: ['Righteous', 'sans-serif'],
+        fredoka: ['Fredoka', 'sans-serif'],
+      },
+      letterSpacing: {
+        tighter: '-0.05em',
+        tight: '-0.025em',
+        normal: '0',
+        wide: '0.025em',
+        wider: '0.05em',
+        widest: '0.1em',
+        ultra: '0.15em',
       },
     },
   },
@@ -39,6 +82,16 @@ module.exports = {
         '.text-emphasis': {
           color: '#d5006d',
           fontWeight: 'bold',
+        },
+        '.text-rotate-small': {
+          transform: 'rotate(-1deg)',
+        },
+        '.text-rotate-large': {
+          transform: 'rotate(-3deg)',
+        },
+        '.text-outline': {
+          textStroke: '1px currentColor',
+          paintOrder: 'stroke fill',
         },
       };
       addUtilities(newUtilities, ['responsive', 'hover']);
